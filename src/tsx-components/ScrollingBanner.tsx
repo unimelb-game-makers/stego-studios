b089950213ff1cf212498563aa19ca4ef32937ac
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2025-03-15 11:56:16 +1100

    Refined scrolling banner

--- src/tsx-components/ScrollingBanner.tsx
+++ src/tsx-components/ScrollingBanner.tsx
@@ -9,11 +9,17 @@ const ScrollingBanner = () => {
         <Marquee className="marquee">
             <div className="announcement">
             <div className="announcement-text text1">
+                <span className="oval"></span>
                 <span>Games this Season</span>
             </div>
             <div className="announcement-text text2">
+                <span className="oval"></span>
                 <span>Games this Season</span>
-
+            </div>
+            
+            <div className="announcement-text text3">
+                <span className="oval"></span>
+                <span>Games this Season</span>
             </div>
         </div>
         </Marquee>
