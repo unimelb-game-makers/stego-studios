c5addda4e46f793446849a4e55e277f64b0cb878
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2025-03-23 16:31:40 +1100

    Added images with games showcase

--- src/tsx-components/Showcase.tsx
+++ src/tsx-components/Showcase.tsx
@@ -8,6 +8,8 @@ const Showcase = () => {
         <div className="top">       
             <div className="topBar">
             <img src="/images/Top_Bar.png" alt="Top_Bar.png"/>
+            <img src="/images/creepycook.png" alt="creepycook.png"/>
+
             </div>
             <div className="topSquare">
             <img src="/images/Top_Square.png" alt="Top_square.png"/>
@@ -17,6 +19,8 @@ const Showcase = () => {
         <div className="middle">
             <div className="middleBar">
             <img src="/images/Middle_Bar.png" alt="Middle_Bar.png"/>
+            <img src="/images/trackline.png" alt="trackline.png"/>
+
             </div>
             <div className="middleSquare">
             <img src="/images/Middle_Square.png" alt="Middle_square.png"/>
@@ -25,14 +29,13 @@ const Showcase = () => {
 
         <div className="bottom">
             <div className="bottomBar">
-
+            <img src="/images/Bottom_Bar.png" alt="Bottom_Bar.png"/>
+            <img src="/images/creatureark.png" alt="creatureark.png"/>
             </div>
-            <div className>
-
+            <div className="bottomSquare">
+            <img src="/images/Bottom_Square.png" alt="Bottom_Square.png"/>
             </div>
         </div>
-
-
         </div>
         
         
