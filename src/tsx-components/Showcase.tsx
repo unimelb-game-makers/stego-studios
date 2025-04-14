57c468d48cb5666642009de2f7177c63c830a503
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2025-04-14 10:49:52 +1000

    commit before major changes

--- src/tsx-components/Showcase.tsx
+++ src/tsx-components/Showcase.tsx
@@ -9,10 +9,25 @@ const Showcase = () => {
             <div className="topBar">
             <img src="/images/Top_Bar.png" alt="Top_Bar.png"/>
             <img src="/images/creepycook.png" alt="creepycook.png"/>
-
             </div>
             <div className="topSquare">
             <img src="/images/Top_Square.png" alt="Top_square.png"/>
+            </div>
+
+            <div className="creepycook">
+                <h1>Creepy Cook!</h1>
+                <p>Cooking | Horror | Roguelike</p>
+                <p>Team Rocket </p>
+                <p>In this survival stealth game, players must scavenge a ruined city</p>
+                <p>for ingredients to cook an offering.  They must avoid monsters</p>
+                <p>while collecting ingredients.</p>
+
+                <div className="creepyButtons">
+                    <button>Learn More</button>
+                    <button>Wishlist on Steam</button>
+                </div>
+                
+
             </div>
         </div>
 
