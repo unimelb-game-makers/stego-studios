d91f189cb5a5f70c368181cd49fd6e61b9d3dcaa
masesuraj112 <157959335+masesuraj112@users.noreply.github.com>
2025-04-21 19:59:26 +1000

    Major changes to showcase section

--- src/tsx-components/Showcase.tsx
+++ src/tsx-components/Showcase.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import '../styles/showcase.css'  // This is the issue
-import ShowcaseItem from "./ShowcaseItem.tsx";
+import '../styles/showcase.css' 
+// import ShowcaseItem from "./ShowcaseItem.tsx";
 
 
 const Showcase = () => {
@@ -9,31 +9,54 @@ const Showcase = () => {
         <div className="showcase-row">
             <div className="showcase-left">
               <img src="/images/Top_Bar.png" alt="Top_Bar.png"/>
-              {/* <img src="/images/creepycook.png" alt="creepycook.png"/> */}
+              <img src="/images/creepycook.png" alt="creepycook.png"/>
             </div>
             <div className="showcase-right">
-              <img src="/images/Top_Square.png" alt="Top_square.png"/>
+            <div className="image-wrapper">
+                <img src="/images/Top_Square.png" alt="Top_square.png" />
+                <div className="overlay-div">
+                    <h1>Creepy Cook!</h1>
+                    <p>Cooking | Horror | Roguelike</p>
+                    <p>Team Rocket</p>
+                    <p>In this survival strength game, players must scavenge a ruined city for ingredients to cook an offering. They must avoid monsters while collecting ingredients.</p>
+                </div>
             </div>
+            
+            </div>
+           
         </div>
 
-        <div className="showcase-row">
+        <div className="showcase-row second-row">
             <div className="showcase-left">
-              <img src="/images/Top_Square.png" alt="Top_square.png"/>
-              {/* <img src="/images/creepycook.png" alt="creepycook.png"/> */}
+
+              <img src="/images/Middle_Square.png" alt="Top_square.png"/>
+              <img src="/images/trackline.png" alt="creepycook.png"/>
             </div>
             <div className="showcase-right">
-              <img src="/images/Top_Bar.png" alt="Top_Bar.png"/>
+              <img src="/images/Middle_Bar.png" alt="Top_Bar.png"/>
+              
             </div>
+          
+            
         </div>
 
 
-        <div className="showcase-row">
+        <div className="showcase-row third-row">
             <div className="showcase-left">
-              <img src="/images/Top_Bar.png" alt="Top_Bar.png"/>
-              {/* <img src="/images/creepycook.png" alt="creepycook.png"/> */}
+              <img src="/images/Bottom_Bar.png" alt="Top_Bar.png"/>
+              <img src="/images/creatureark.png" alt="creepycook.png"/>
             </div>
             <div className="showcase-right">
-              <img src="/images/Top_Square.png" alt="Top_square.png"/>
+              <div className="image-wrapper">
+                <img src="/images/Bottom_Square.png" alt="Top_square.png" />
+                <div className="overlay-div">
+                    <h1>Creepy Cook!</h1>
+                    <p>Cooking | Horror | Roguelike</p>
+                    <p>Team Rocket</p>
+                    <p>In this survival strength game, players must scavenge a ruined city for ingredients to cook an offering. They must avoid monsters while collecting ingredients.</p>
+                </div>
+            </div>
+
             </div>
         </div>
       </div>
