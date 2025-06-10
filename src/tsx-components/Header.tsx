c361a04fdd3de4440497a8d26980e4702b4a0a12
Alex Kam <kamzhanyue@gmail.com>
2025-06-10 16:54:27 +0900

    fix header

--- src/tsx-components/Header.tsx
+++ src/tsx-components/Header.tsx
@@ -1,35 +1,23 @@
-import React from "react";
-import '../styles/header.css'; 
-import Navbar from './Nav.tsx';
-
-import { BrowserRouter } from "react-router-dom";
-
-
+import "../styles/header.css";
+import Navbar from "./Nav.tsx";
 
 const Header = () => {
-    return (
-        
-        <header>
-            <div class="left">
-            <img src="/images/stego.png" alt="Stego.png"/>
-            <h1> Stego Studios </h1>
-            </div>
+  return (
+    <header>
+      <div className="left">
+        <img src="/images/stego.png" alt="Stego.png" />
+        <h1> Stego Studios </h1>
+      </div>
 
-            <div class="right">
-            {/* <BrowserRouter>
-            <Navbar />
-            </BrowserRouter> */}
-            <Navbar />
-            <div class="logo">
-            <img src="/images/discord_logo.png" alt="Discord.png"/>
-            <img src="/images/email_logo.png" alt="Grey-email.jpg"/>
-            </div>
-            </div>
-        
-        </header>
+      <div className="right">
+        <Navbar />
+        <div className="logo">
+          <img src="/images/discord_logo.png" alt="Discord.png" />
+          <img src="/images/email_logo.png" alt="Grey-email.jpg" />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-
-    )
-}
-
-export default Header
+export default Header;
