0be99c9e7e87fe6adbeacf2f8e75cbd2e47e9d71
Alex Kam <kamzhanyue@gmail.com>
2025-06-15 15:45:28 +0900

    feat: add highlight for current page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,17 +1,36 @@
-import { Link } from "@tanstack/react-router";
+import { Link, useMatch } from "@tanstack/react-router";
 import "../styles/navbar.css";
 
 const Navbar = () => {
+  const isTeamActive = useMatch({ from: "/_layout/team", shouldThrow: false });
+  const isGamesActive = useMatch({
+    from: "/_layout/games/",
+    shouldThrow: false,
+  });
+  const isContactActive = useMatch({
+    from: "/_layout/contact",
+    shouldThrow: false,
+  });
+
   return (
     <div>
       <nav>
-        <Link to="/team" className="nav-link">
+        <Link
+          to="/team"
+          className={`nav-link ${isTeamActive ? "nav-link-current" : ""}`}
+        >
           Team
         </Link>
-        <Link to="/games" className="nav-link">
+        <Link
+          to="/games"
+          className={`nav-link ${isGamesActive ? "nav-link-current" : ""}`}
+        >
           Games
         </Link>
-        <Link to="/contact" className="nav-link">
+        <Link
+          to="/contact"
+          className={`nav-link ${isContactActive ? "nav-link-current" : ""}`}
+        >
           Contact
         </Link>
       </nav>
