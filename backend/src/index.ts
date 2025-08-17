3d7b9e0a4057a9322856c297a1ddad05ceecc1b3
kamzhanyue <kamzhanyue@gmail.com>
2025-08-17 13:10:45 +1000

    chore: setup notion sdk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,10 @@
 import express from "express";
+import { Client } from "@notionhq/client"
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +13,8 @@ app.get("/", (_req, res) => {
   res.send("Hello from Bun + Express + TypeScript!");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`🚀 Server running at http://localhost:${port}`);
+  const listUsersResponse = await notion.users.list({})
+  console.log(listUsersResponse)
 });
