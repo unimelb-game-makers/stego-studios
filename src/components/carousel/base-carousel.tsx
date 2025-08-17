82479683c9d9372b8cb21f28c80191f46452a435
kamzhanyue <kamzhanyue@gmail.com>
2025-08-17 10:55:00 +1000

    chore: remove material icons

--- src/components/carousel/base-carousel.tsx
+++ src/components/carousel/base-carousel.tsx
@@ -5,19 +5,25 @@ import { usePrevNextButtons } from "./carousel-buttons.tsx";
 import "../../styles/embla.css";
 import "../../styles/carousel.css";
 
-import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+// import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const OPTIONS = { loop: true, duration: 30 };
 
 interface Props {
-  images: string[],
-  viewportClass: string,
-  containerClass: string,
-  slideClass: string,
-  slideImageClass: string,
+  images: string[];
+  viewportClass: string;
+  containerClass: string;
+  slideClass: string;
+  slideImageClass: string;
 }
 
-const BaseCarousel = ({ images, viewportClass, containerClass, slideClass, slideImageClass }: Props) => {
+const BaseCarousel = ({
+  images,
+  viewportClass,
+  containerClass,
+  slideClass,
+  slideImageClass,
+}: Props) => {
   const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Fade()]);
   const { onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
   return (
@@ -25,22 +31,18 @@ const BaseCarousel = ({ images, viewportClass, containerClass, slideClass, slide
       <div className={containerClass}>
         {images.map((src, index) => (
           <div className={slideClass} key={index}>
-            <img
-              className={slideImageClass}
-              src={src}
-              alt="Carousel Slide"
-            />
+            <img className={slideImageClass} src={src} alt="Carousel Slide" />
           </div>
         ))}
       </div>
 
-      <ArrowBackIos className="arrow-back" onClick={onPrevButtonClick} />
+      {/* <ArrowBackIos className="arrow-back" onClick={onPrevButtonClick} />
       <ArrowForwardIos
         className="arrow-forward"
         onClick={onNextButtonClick}
-      />
+      /> */}
     </div>
   );
 };
 
-export { BaseCarousel }
+export { BaseCarousel };
