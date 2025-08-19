8e2b38a70feecd8f5268eac58b14c62c979b665d
Qi Han <tan.qihan1@gmail.com>
2025-08-19 23:45:45 +1000

    fix: un-commented arrow buttons

--- frontend/src/components/carousel/base-carousel.tsx
+++ frontend/src/components/carousel/base-carousel.tsx
@@ -5,44 +5,41 @@ import { usePrevNextButtons } from "./carousel-buttons.tsx";
 import "../../styles/embla.css";
 import "../../styles/carousel.css";
 
-// import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
 
 const OPTIONS = { loop: true, duration: 30 };
 
 interface Props {
-  images: string[];
-  viewportClass: string;
-  containerClass: string;
-  slideClass: string;
-  slideImageClass: string;
+	images: string[];
+	viewportClass: string;
+	containerClass: string;
+	slideClass: string;
+	slideImageClass: string;
 }
 
 const BaseCarousel = ({
-  images,
-  viewportClass,
-  containerClass,
-  slideClass,
-  slideImageClass,
+	images,
+	viewportClass,
+	containerClass,
+	slideClass,
+	slideImageClass,
 }: Props) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Fade()]);
-  const { onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
-  return (
-    <div className={viewportClass} ref={emblaRef}>
-      <div className={containerClass}>
-        {images.map((src, index) => (
-          <div className={slideClass} key={index}>
-            <img className={slideImageClass} src={src} alt="Carousel Slide" />
-          </div>
-        ))}
-      </div>
+	const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [Fade()]);
+	const { onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
+	return (
+		<div className={viewportClass} ref={emblaRef}>
+			<div className={containerClass}>
+				{images.map((src, index) => (
+					<div className={slideClass} key={index}>
+						<img className={slideImageClass} src={src} alt="Carousel Slide" />
+					</div>
+				))}
+			</div>
 
-      {/* <ArrowBackIos className="arrow-back" onClick={onPrevButtonClick} />
-      <ArrowForwardIos
-        className="arrow-forward"
-        onClick={onNextButtonClick}
-      /> */}
-    </div>
-  );
+			<ArrowBackIos className="arrow-back" onClick={onPrevButtonClick} />
+			<ArrowForwardIos className="arrow-forward" onClick={onNextButtonClick} />
+		</div>
+	);
 };
 
 export { BaseCarousel };
