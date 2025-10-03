c632599f6e718558ce4de98f18a07431fa67751f
iden0605 <79390850+iden0605@users.noreply.github.com>
2025-10-03 16:30:20 +1000

    mobile styles implemented

--- src/routes/_layout/team/$team.$member.tsx
+++ src/routes/_layout/team/$team.$member.tsx
@@ -22,7 +22,7 @@ function RouteComponent() {
 
   const memberData = {
     name: "James Conrad",
-    skills: "Game Design, Level Design, System Design",
+    skills: ["Game Design", "Level Design", "System Design"],
     contact: ["discord", "discord", "email"],
     description:
       "James Conrad is a senior game designer with over 10 years of experience. He is the creative force behind the game's mechanics and flow. He thrives on finding the sweet spot where strategy meets fun, shaping systems that challenge players without overwhelming them. With a love for puzzle-solving and world-building, James makes sure every feature connects to the bigger picture of the game. When he's not refining levels, he's usually testing new board games or sketching out his next big idea.",
@@ -36,23 +36,33 @@ function RouteComponent() {
 
         <div className="member-avatar-container">
           <div className="member-avatar" />
+          <h1 className="mobile-name">{memberData.name}</h1>
           <p className="member-role">{memberData.role}</p>
         </div>
 
         <div className="member-card-info">
-          <h1>{memberData.name}</h1>
+          <h1 className="desktop-name">{memberData.name}</h1>
           <div className="member-details">
-            <p>
-              <strong>skills:</strong> {memberData.skills}
-            </p>
+            <div className="member-skills">
+              <strong>skills:</strong>
+              <div className="skills-container">
+                {memberData.skills.map((skill) => (
+                  <span key={skill} className="skill-tag">
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </div>
             <div className="member-contact">
               <strong>contact:</strong>
-              <a href="#" className="discord-contact-item">
-                <img src="/images/discord_logo.png" alt="Discord" />
-              </a>
-              <a href="#" className="email-contact-item">
-                <img src="/images/email_logo.png" alt="Email" />
-              </a>
+              <div className="contact-icons">
+                <a href="#" className="discord-contact-item">
+                  <img src="/images/discord_logo.png" alt="Discord" />
+                </a>
+                <a href="#" className="email-contact-item">
+                  <img src="/images/email_logo.png" alt="Email" />
+                </a>
+              </div>
             </div>
             <p>
               <strong>personal note:</strong>
